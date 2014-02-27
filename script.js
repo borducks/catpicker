3254eeed3f077ee0d6ca8b52af9fef4863d986e6
Paul Bordeaux <paul.bordeaux@answers.com>
2014-02-26 23:31:13 -0600

    got second level to behave. all of the data attribs are making me cross-eyed.

--- script.js
+++ script.js
@@ -2,26 +2,47 @@ $(document).ready(function (event) {
 	// body...
 	
 	// assign event handlers
-	$('.category.level_1 .category_tile').on('click', changeToColumnMode);
+	$('.category .category_tile').on('click', handleCategoryClick);
 	
 	
 	
 	// funcs
-	
-	function changeToColumnMode(event) {
-		// body...
-		var tile = $(event.target).parents('.category_tile');
-		console.log($(event.target).parents('.category_tile'));
+	function handleCategoryClick(event) {
+		// category_tile is target
 		
-		category.parent().removeClass('floated');
+		//switch to column view
+		changeToColumnMode($(event.target));
+		
+		// display subcategories of target
+		initSubcategory($(event.target).data('category-name'));
+	}
+	
+	function changeToColumnMode(tile) {
+		// switch from grid to column view
+		hideAllChildren(tile.data('category-level'));
+		tile.parent().removeClass('floated');
+		// console.log(tile.data('category-name'));
 	}
 	
 	// queue up next category
+	function initSubcategory(cat_name) {
+		console.log('reveal children of [data-category-parent="'+cat_name+'"]');
+		console.log($('.category[data-category-parent="'+cat_name+'"]'));
+		$('.category[data-category-parent="'+cat_name+'"]').addClass('floated').show();
+	}
 	
+	function hideAllChildren(level) {
+		console.log('hideAllChildren past level '+level);
+		for (var i = level+1; i < 5; i++) {
+			$('.category.level_'+i).hide();
+		}
+	}
+		
 	// add tile to selected
 	
 	// remove tile from selected
 	
 	
+
 	
 })
